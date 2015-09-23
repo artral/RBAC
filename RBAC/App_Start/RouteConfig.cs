87931020feed25ebc8e451f9eb51b8fdfbef3a94
Artral <artralarasu@gmail.com>
2015-09-22 21:52:40 -0500

    action

--- RBAC/App_Start/RouteConfig.cs
+++ RBAC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RBAC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "user", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
